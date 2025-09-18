1de9d988772e2d343b622fe5e7eccc41f7e49b10
Arthur Gabriel Gotardo Pohl <122641048+Fear-sys@users.noreply.github.com>
2025-09-18 16:58:47 -0300

    Beta 2.0

--- script_lista.js
+++ script_lista.js
@@ -1,10 +1,33 @@
+
+//contador de clicks 
+let contadorProdutos = {
+    Cookie: 0,
+    brownie: 0
+    // Adicione mais produtos conforme necessário
+};
+
+
+
+
+
+
+
+
 function comprarProduto(botao){
-    const numeroPedido = Math.floor(Math.random() * 1000) + 1;
+    //Pegar nome do produto
     const nomeProduto =botao.getAttribute('nome');
+
+
+
+   //Adicionar um clik ao produto
+    contadorProdutos[nomeProduto]++;
+   
+   //criar mensagem
+    const numeroPedido = Math.floor(totaldecliques+ 1);
     const whatsinsa = "5549988183883"
     const mensagem = `O número do pedido é ${numeroPedido} e o produto é ${nomeProduto}`;
     const linkWhatsApp = `https://wa.me/${encodeURIComponent(whatsinsa)}?text=${encodeURIComponent(mensagem)}`;
     
-
+    //mandar para whatsapp
     window.open(linkWhatsApp, '_blank');
 }
