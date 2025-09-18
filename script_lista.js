94d32f0066f5d639d09ba43f52a50d295dbcf375
Arthur Gabriel Gotardo Pohl <122641048+Fear-sys@users.noreply.github.com>
2025-09-18 16:47:41 -0300

    Beta 1.1

--- script_lista.js
+++ script_lista.js
@@ -1,9 +1,9 @@
 function comprarProduto(botao){
     const numeroPedido = Math.floor(Math.random() * 1000) + 1;
-    const nomeProduto =botao.getAtrubute(nome)
-    const whatsinsa = 5549988183883
-    const mensagem = `O número do pedido é ${numeroPedido} e o produto é ${nomeproduto}`;
-    const linkWhatsApp = `https://wa.me/${encodeURIComponent(Wahtsinsa)}?text=${encodeURIComponent(mensagem)}`;
+    const nomeProduto =botao.getAttribute('nome');
+    const whatsinsa = "5549988183883"
+    const mensagem = `O número do pedido é ${numeroPedido} e o produto é ${nomeProduto}`;
+    const linkWhatsApp = `https://wa.me/${encodeURIComponent(whatsinsa)}?text=${encodeURIComponent(mensagem)}`;
     
 
     window.open(linkWhatsApp, '_blank');
